1f6220c4bb2c97777e1a5f8ea35edb3221337b24
Ollie Carson <ojc240000@utdallas.edu>
2026-04-21 16:43:33 -0500

    created hitbox and made it visible.

--- build_burger.py
+++ build_burger.py
@@ -3,6 +3,16 @@ import os
 import pygame
 from PIL import Image
 
+def build():
+    bottom_bun = pygame.image.load("Burger_Game_Assets/burger_build/Build_Bottom_Bun").convert_alpha()
+    screen.blit(bottom_bun, (632, 815))
+    #Make hitbox
+    b_bun_rect = bottom_bun.get_rect(topleft = (632, 815))
+    bun_hitbox = b_bun_rect.inflate(-20, -20)
+
+# to see my hitbox for changing
+    pygame.draw.rect(screen, (255, 0, 0), bun_hitbox, 2)
+
 def main():
     pygame.init()
     pygame.display.set_caption("Build Burger")
@@ -15,6 +25,7 @@ def main():
                 running = False
                 
         screen.blit(bg, (0, 0))
+        build(screen)
         pygame.display.flip()
 
     pygame.quit()
