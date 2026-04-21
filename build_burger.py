cfb8ab5c1e98686f610d3445c7754dae4699613d
Ollie Carson <ojc240000@utdallas.edu>
2026-04-21 15:57:27 -0500

    Imported my image folder and set the background

---
+++ build_burger.py
@@ -0,0 +1,36 @@
+import random
+import pygame
+from PIL import Image
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Build Burger")
+    screen = pygame.display.set_mode((1920, 1080))
+    bg = pygame.image.load("Burger_BG.png")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
