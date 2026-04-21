ce02caef5432a4467e5cfd3b7d0c5a429cfe4f77
Ollie Carson <ojc240000@utdallas.edu>
2026-04-21 18:26:40 -0500

    Randomly picks and displays a ingrediant with a hitbox. too fast.

--- build_burger.py
+++ build_burger.py
@@ -11,23 +11,29 @@ def get_random_image(folder_path):
     return pygame.image.load(os.path.join(folder_path, random_image)).convert_alpha()
 
 
-
-
-
-
-
-
-
-
 def build(screen):
     bottom_bun = pygame.image.load("Burger_Game_Assets/Build_Bottom_Bun.png").convert_alpha()
     #Make hitbox
     b_bun_rect = bottom_bun.get_rect(topleft = (632, 815))
     bun_hitbox = b_bun_rect.inflate(-20, -90)
 
-# to see my hitbox for changing
     screen.blit(bottom_bun, (632, 815))
     pygame.draw.rect(screen, (255, 0, 0), bun_hitbox, 2)
+
+
+def display_random_ing(screen, folder_path):
+    random_ing = get_random_image(folder_path)
+    if random_ing:
+        ing_rect = random_ing.get_rect(center = (960, 100))
+        ing_hitbox = ing_rect.inflate(-20, -90)
+
+        screen.blit(random_ing, ing_rect)
+        pygame.draw.rect(screen, (0, 255, 0), ing_hitbox, 2)
+
+
+
+
+
 
 def main():
     pygame.init()
@@ -35,6 +41,7 @@ def main():
     screen = pygame.display.set_mode((1920, 1080))
     bg = pygame.image.load("Burger_Game_Assets/Burger_BG.png").convert()
     clock = pygame.time.Clock()
+    folder_path = os.path.join("Burger_Game_Assets", "burger_build")
     
     running = True
     while running:
@@ -44,6 +51,7 @@ def main():
                 
         screen.blit(bg, (0, 0))
         build(screen)
+        display_random_ing(screen, folder_path)
 
         pygame.display.flip()
         clock.tick(24)
